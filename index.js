6a9e1ab46e8f983af6659632ea1572b1958584da
Raphael Mazzieri <rquitto@gmail.com>
2023-11-30 20:37:25 -0300

    "refatorado todo o codigo"

--- index.js
+++ index.js
@@ -1,82 +1,129 @@
 const botaoConverter = document.querySelector(".botao");
 
-const seletorDeMoedasParaConverter = document.querySelector(".seletor-moedas-converter");
+const primeiroSeletorDeMoedas = document.querySelector(".seletor-moedas-converter");
 
-const seletorDeMoedasConvertidas = document.querySelector(".seletor-moedas-convertidas");
+const segundoSeletorDeMoedas = document.querySelector(".seletor-moedas-convertidas");
 
+const valorDigitadoPeloUsuario = document.querySelector(".input-valor").value;
 
-function converterMoeda(){
-
-    const valorDigitadoPeloUsuario = document.querySelector(".input-valor").value;
+function converterMoeda() {
     
-    const dolar = 4.91;
-    const euro = 5.36;
-    let moedaAconverter = valorDigitadoPeloUsuario;
-    const realComDolar = moedaAconverter * dolar;
-    // const realComEuro = moedaAconverter * euro;
 
+    const real = 1.00;
+    const dolarEmReal = 4.93;
+    const euroEmReal = 5.36;
 
-    const valoraConverter = document.querySelector(".campo-valor-converter");
-    
-    if(seletorDeMoedasParaConverter.value == "dolar"){
-        valoraConverter.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(realComDolar);
-        
-     } // else if(seletorDeMoedasParaConverter.value == "euro"){
-    //     valoraConverter.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(realComEuro);
-    // }
+    const dolarEmEuro = 1.09;
+    const euroEmDolar = 0.92;
 
 
     const valorConvertido = document.querySelector(".campo-valor-convertido");
+    const valorConverter = document.querySelector(".campo-valor-converter");
 
 
-    if(seletorDeMoedasConvertidas.value == "dolar"){
-        valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario / dolar);
+    if (primeiroSeletorDeMoedas.value == "real" && segundoSeletorDeMoedas.value == "dolar") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario / dolarEmReal);
 
-    } else if(seletorDeMoedasConvertidas.value == "euro"){
-        valorConvertido.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario / euro);
+        valorConverter.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario);
 
-    } else if (seletorDeMoedasConvertidas.value == "real"){
-        valorConvertido.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(realComDolar);
+
+    } else if (primeiroSeletorDeMoedas.value == "dolar" && segundoSeletorDeMoedas.value == "real") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario * dolarEmReal);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario);
+
+
+    } else if (primeiroSeletorDeMoedas.value == "real" && segundoSeletorDeMoedas.value == "euro") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario / euroEmReal);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario);
+
+
+    } else if (primeiroSeletorDeMoedas.value == "euro" && segundoSeletorDeMoedas.value == "real") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario * euroEmReal);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario);
+
+
+    } else if (primeiroSeletorDeMoedas.value == "dolar" && segundoSeletorDeMoedas.value == "euro") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario / dolarEmEuro);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario);
+
+
+    } else if (primeiroSeletorDeMoedas.value == "euro" && segundoSeletorDeMoedas.value == "dolar") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario / euroEmDolar);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario);
+
+    } else if (primeiroSeletorDeMoedas.value == "real" && segundoSeletorDeMoedas.value == "real") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario);
+
+
+    } else if (primeiroSeletorDeMoedas.value == "dolar" && segundoSeletorDeMoedas.value == "dolar") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valorDigitadoPeloUsuario);
+
+    } else if (primeiroSeletorDeMoedas.value == "euro" && segundoSeletorDeMoedas.value == "euro") {
+        valorConvertido.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario);
+
+        valorConverter.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(valorDigitadoPeloUsuario);
     }
 
-
-    valoraConverter.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorDigitadoPeloUsuario);
-    
-    
 }
 
-function mudarPaisMoedaConvertida(){
-    const nomeMoedaConverter = document.querySelector(".campo-moeda-converter");
-    const imagemPaisConverter = document.querySelector(".imagem-pais-converter");
+function mudarPaisMoedaConverter(){
+    const campoMoedaConverter = document.querySelector(".campo-moeda-converter");
+    const imagemPaisEsquerda = document.querySelector(".imagem-pais-esquerda");
 
-    if(seletorDeMoedasParaConverter.value == "dolar"){
-        nomeMoedaConverter.innerHTML = "USD";
-        imagemPaisConverter.src = "./src/imagens/logo-dolar.png";
+    if (primeiroSeletorDeMoedas.value == "dolar"){
+
+        campoMoedaConverter.innerHTML = "USD";
+        imagemPaisEsquerda.src = "./src/imagens/logo-dolar.png";
+
+    } else if (primeiroSeletorDeMoedas.value == "euro"){
+
+        campoMoedaConverter.innerHTML = "EUR";
+        imagemPaisEsquerda.src = "./src/imagens/logo-euro.png";
+
+    } else if (primeiroSeletorDeMoedas.value == "real"){
+
+        campoMoedaConverter.innerHTML = "BRL";
+        imagemPaisEsquerda.src = "./src/imagens/logo-brl.png";
     }
 
+}
 
-    const nomeMoedaConvertida = document.querySelector(".campo-moeda-convertida");
-    const imagemPaisConvertido = document.querySelector(".imagem-pais-convertido");
+function mudarPaisMoedaConvertida() {
+    const campoMoedaConvertida = document.querySelector(".campo-moeda-convertida");
+    const imagemPaisDireita = document.querySelector(".imagem-pais-direita");
+
+    if (segundoSeletorDeMoedas.value == "dolar") {
+
+        campoMoedaConvertida.innerHTML = "USD";
+        imagemPaisDireita.src = "./src/imagens/logo-dolar.png";
 
 
-    if(seletorDeMoedasConvertidas.value == "dolar"){
-        nomeMoedaConvertida.innerHTML = "USD";
-        imagemPaisConvertido.src = "./src/imagens/logo-dolar.png";
+    } else if (segundoSeletorDeMoedas.value == "euro") {
 
-    } else if(seletorDeMoedasConvertidas.value == "euro"){
-        nomeMoedaConvertida.innerHTML = "EUR";
-        imagemPaisConvertido.src = "./src/imagens/logo-euro.png";
+        campoMoedaConvertida.innerHTML = "EUR";
+        imagemPaisDireita.src = "./src/imagens/logo-euro.png"
 
-    } else if ( seletorDeMoedasConvertidas.value == "real"){
-        nomeMoedaConvertida.innerHTML = "BRL";
-        imagemPaisConvertido.src = "./src/imagens/logo-brl.png";
 
+    } else if (segundoSeletorDeMoedas.value == "real") {
+
+        campoMoedaConvertida.innerHTML = "BRL";
+        imagemPaisDireita.src = "./src/imagens/logo-brl.png";
     }
 
     converterMoeda();
+
 }
 
 
-seletorDeMoedasConvertidas.addEventListener("change", mudarPaisMoedaConvertida);
+primeiroSeletorDeMoedas.addEventListener("change", mudarPaisMoedaConverter)
+segundoSeletorDeMoedas.addEventListener("change", mudarPaisMoedaConvertida);
 botaoConverter.addEventListener("click", converterMoeda);
-
